766fa65d181305ced5d587bf9bf537fcc971ea51
miracleren <miracleren@gmail.com>
2024-09-06 17:19:27 +0800

    init

---
+++ backend/ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -0,0 +1,27 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ *
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RuoYiApplication {
+    public static void main(String[] args) {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n\n" +
+                "  /$$$$$$                      \n" +
+                " /$$__  $$                     \n" +
+                "| $$  \\ $$ /$$$$$$$   /$$$$$$  \n" +
+                "| $$  | $$| $$__  $$ /$$__  $$ \n" +
+                "| $$  | $$| $$  \\ $$| $$$$$$$$ \n" +
+                "| $$  | $$| $$  | $$| $$_____/ \n" +
+                "|  $$$$$$/| $$  | $$|  $$$$$$$ \n" +
+                " \\______/ |__/  |__/ \\_______/ \n");
+    }
+}
